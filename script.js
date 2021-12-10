48b1fa17f1e22d299c9652baf83402603ba483ad
Otto Micheletti <michelettiotto@gmail.com>
2021-12-10 03:08:47 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ window.onload = function () {
   // Requisito 6
   function blackStartsSelected() {
     let black = document.querySelector('#color1');
-    black.classList.add('selected');
+    black.className = 'color selected';
   }
 
   blackStartsSelected();
 
   // Requisito 12 - Randomiza cores da paleta
   function randomColors() {
-    const colors = document.getElementsByClassName('color');
+    const colors = document.querySelectorAll('.color');
     let numbers = [];
     for (let index = 0; index <= 8; index += 1) {
       numbers.push(Math.floor(Math.random() * 256));
@@ -28,44 +28,55 @@ window.onload = function () {
     colors[3].style.backgroundColor = `rgb(${randomColors.rgb4})`;
   }
   randomColors();
-
-  // Requisito 7 - Clicar na cor, seleciona para utilizar.
-  function selectColor() {
-    const color1 = document.getElementById('color1');
-    const color2 = document.getElementById('color2');
-    const color3 = document.getElementById('color3');
-    const color4 = document.getElementById('color4');
-
-    color1.addEventListener('click', function () {
-      color1.className = 'color selected';
-      color2.className = 'color';
-      color3.className = 'color';
-      color4.className = 'color';
-    });
-
-    color2.addEventListener('click', function () {
-      color2.className = 'color selected';
-      color1.className = 'color';
-      color3.className = 'color';
-      color4.className = 'color';
-    });
-
-    color3.addEventListener('click', function () {
-      color3.className = 'color selected';
-      color1.className = 'color';
-      color2.className = 'color';
-      color4.className = 'color';
-    });
-
-    color4.addEventListener('click', function () {
-      color4.className = 'color selected';
-      color1.className = 'color';
-      color2.className = 'color';
-      color3.className = 'color';
-    });
-  }
-
-  selectColor();
-
-  // Requisito X
 };
+
+// Requisito 7 - Clicar na cor, seleciona para utilizar.
+function selectColor() {
+  const color1 = document.getElementById('color1');
+  const color2 = document.getElementById('color2');
+  const color3 = document.getElementById('color3');
+  const color4 = document.getElementById('color4');
+
+  color1.addEventListener('click', function () {
+    color1.className = 'color selected';
+    color2.className = 'color';
+    color3.className = 'color';
+    color4.className = 'color';
+  });
+
+  color2.addEventListener('click', function () {
+    color2.className = 'color selected';
+    color1.className = 'color';
+    color3.className = 'color';
+    color4.className = 'color';
+  });
+
+  color3.addEventListener('click', function () {
+    color3.className = 'color selected';
+    color1.className = 'color';
+    color2.className = 'color';
+    color4.className = 'color';
+  });
+
+  color4.addEventListener('click', function () {
+    color4.className = 'color selected';
+    color1.className = 'color';
+    color2.className = 'color';
+    color3.className = 'color';
+  });
+}
+
+selectColor();
+
+// Requisito 8 - Pinta o pixel com a cor escolhida
+function paintPixels() {
+  document.addEventListener('click', function (event) {
+    const colorDiv = document.querySelector('.selected').getAttribute('style');
+
+    if (event.target.classList.contains('pixel')) {
+      event.target.style = colorDiv;
+    }
+    // console.log(colorDiv);
+  });
+}
+paintPixels();
