de8442c66833a89da0eff7dc8f15ce97e7278354
Otto Micheletti <michelettiotto@gmail.com>
2021-12-09 23:10:52 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,50 +1,71 @@
 window.onload = function () {
-  let pallete = document.getElementsByClassName('color');
-  let color1 = document.getElementById('color1');
-  let color2 = document.getElementById('color2');
-  let color3 = document.getElementById('color3');
-  let color4 = document.getElementById('color4');
-
-  color1.classList.add('selected');
-  console.log(pallete);
-  // Requisito 7 - Clicar na cor, seleciona para utilizar.
-  // function colorPick() {
-  //   let selected = 'selected';
-  //   let empty = '';
-
-  //   pallete.addEventListener('click', function (event) {
-  //     event.target.classList.add = selected;
-  //   });
-
-  //   for (let index = 0; index < pallete.length; index += 1) {
-  //     if (pallete[index].className === selected) {
-
-  //     }
-  //   }
-  // }
-
-  // colorPick();
-
-  // Bônus - Randomiza cores
-
-  let numbers = [];
-  for (let index = 0; index <= 8; index += 1) {
-    numbers.push(Math.ceil(Math.random() * 255));
+  // Requisito 6
+  function blackStartsSelected() {
+    let black = document.querySelector('#color1');
+    black.classList.add('selected');
   }
 
-  let randomColors = {
-    rgb1: '0, 0, 0',
-    rgb2: numbers.slice(0, 3).join(', '),
-    rgb3: numbers.slice(3, 6).join(', '),
-    rgb4: numbers.slice(6, 9).join(', '),
-  };
+  blackStartsSelected();
 
-  document.getElementById('color1').style.backgroundColor =
-    'rgb(' + randomColors.rgb1 + ')';
-  document.getElementById('color2').style.backgroundColor =
-    'rgb(' + randomColors.rgb2 + ')';
-  document.getElementById('color3').style.backgroundColor =
-    'rgb(' + randomColors.rgb3 + ')';
-  document.getElementById('color4').style.backgroundColor =
-    'rgb(' + randomColors.rgb4 + ')';
+  // Requisito 12 - Randomiza cores da paleta
+  function randomColors() {
+    const colors = document.getElementsByClassName('color');
+    let numbers = [];
+    for (let index = 0; index <= 8; index += 1) {
+      numbers.push(Math.floor(Math.random() * 256));
+    }
+
+    let randomColors = {
+      rgb1: '0, 0, 0',
+      rgb2: numbers.slice(0, 3).join(', '),
+      rgb3: numbers.slice(3, 6).join(', '),
+      rgb4: numbers.slice(6, 9).join(', '),
+    };
+
+    colors[0].style.backgroundColor = `rgb(${randomColors.rgb1})`;
+    colors[1].style.backgroundColor = `rgb(${randomColors.rgb2})`;
+    colors[2].style.backgroundColor = `rgb(${randomColors.rgb3})`;
+    colors[3].style.backgroundColor = `rgb(${randomColors.rgb4})`;
+  }
+  randomColors();
+
+  // Requisito 7 - Clicar na cor, seleciona para utilizar.
+  function selectColor() {
+    const color1 = document.getElementById('color1');
+    const color2 = document.getElementById('color2');
+    const color3 = document.getElementById('color3');
+    const color4 = document.getElementById('color4');
+
+    color1.addEventListener('click', function () {
+      color1.className = 'color selected';
+      color2.className = 'color';
+      color3.className = 'color';
+      color4.className = 'color';
+    });
+
+    color2.addEventListener('click', function () {
+      color2.className = 'color selected';
+      color1.className = 'color';
+      color3.className = 'color';
+      color4.className = 'color';
+    });
+
+    color3.addEventListener('click', function () {
+      color3.className = 'color selected';
+      color1.className = 'color';
+      color2.className = 'color';
+      color4.className = 'color';
+    });
+
+    color4.addEventListener('click', function () {
+      color4.className = 'color selected';
+      color1.className = 'color';
+      color2.className = 'color';
+      color3.className = 'color';
+    });
+  }
+
+  selectColor();
+
+  // Requisito X
 };
